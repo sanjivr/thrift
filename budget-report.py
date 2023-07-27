13f5889229c711718f178fb6832b7b4c7b5f0b7d
Sanjiv Raj <sanjiv.raj@gmail.com>
2023-07-27 18:27:24 -0500

    Output report to html file; Ability to run outside notebook

---
+++ budget-report.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[155]:
+
+
+import pandas as pd
+from datetime import datetime
+import gspread
+
+
+# In[156]:
+
+
+def is_notebook() -> bool:
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            #print("Jupyter notebook or qtconsole")
+            return True
+        elif shell == 'TerminalInteractiveShell':
+            #print("Terminal running IPython")
+            return False
+        else:
+            #print("Other type (?)")
+            return False
+    except NameError:
+        #print("Probably standard")
+        return False
+    
+if is_notebook():
+    import IPython
+    HTML = IPython.display.HTML
+
+
+# In[157]:
+
+
+gc = gspread.service_account(filename='service_account.json')
+
+years = []
+years.append(datetime.now().year)
+#years.append(2022)
+
+rdfs = []
+for year in years:
+    sh = gc.open(str(year))
+    worksheet = sh.worksheet('USD')
+    rdfs.append(pd.DataFrame(worksheet.get_all_records()))
+    #rdfs.append(pd.read_excel(f'~/playground/Expenses/{year}.xlsx', sheet_name="USD"))
+
+
+
+# In[158]:
+
+
+rdf = pd.concat(rdfs)
+
+rdf["Merchant"] = rdf["Merchant"].str.strip()
+rdf['Date'] =  pd.DatetimeIndex(rdf.Date)
+rdf = rdf.drop(columns=["Notes", "Description"])
+
+
+# In[159]:
+
+
+credits = rdf[(rdf["Transaction Type"] == "credit") & (rdf["Category"] != "Salary")]
+credits = credits.assign(Amount=lambda x: x.Amount * -1)
+credits['Amount'].sum()
+
+
+# In[160]:
+
+
+debits = rdf[rdf["Transaction Type"] == "debit"]
+debits['Amount'].sum()
+
+
+# In[161]:
+
+
+df = pd.concat([debits, credits])
+df = df.assign(parsed=df['Category'].str.split('+')).explode('parsed')
+df = df.drop(columns="Category")
+#df
+
+
+# In[162]:
+
+
+def category(parsed):
+    return parsed.split('(').str[0].str.strip()
+
+
+# In[163]:
+
+
+def amount(parsed):
+    return parsed.split('(').str[1]
+
+
+# In[164]:
+
+
+df = df.assign(Category=lambda x: category(x.parsed.str))
+df = df.assign(RAmount=lambda x: amount(x.parsed.str))
+
+df['RAmount'] = df['RAmount'].str.split(')').str[0].astype('float')
+df['RAmount'].fillna(df['Amount'], inplace=True)
+df = df.drop(columns='parsed')
+
+
+# In[165]:
+
+
+monthly_report = df.groupby([df.Date.dt.to_period('M'), "Category"])['RAmount'].sum().reset_index(name='Amount').sort_values(by=['Date', 'Amount'], ascending=False)
+monthly_report['Amount'].sum()
+#monthly_report
+
+
+# In[166]:
+
+
+pv_table = pd.pivot_table(monthly_report, index = 'Category', columns = 'Date', values = 'Amount', fill_value=0, dropna=False)
+#fig = pv_table.plot(kind = 'bar', figsize=(20,10),  yticks=(100, 250, 500, 1000, 1500, 2000, 2500, 5000))
+
+
+# In[167]:
+
+
+#fig.get_figure().savefig("monthly_category_report.png", format="png")
+
+
+# In[168]:
+
+
+category_report = df.groupby(["Category"])['RAmount'].sum().reset_index(name='Amount').sort_values(by=['Amount'], ascending=False)
+#with pd.option_context("display.max_rows", 1000):
+#    display(HTML(category_report.to_html(index=False, header=True)))
+
+
+# In[169]:
+
+
+annual_monthly_report=category_report.merge(pv_table, on='Category')
+
+annual_monthly_report.loc['Total'] = annual_monthly_report.sum(numeric_only=True)
+annual_monthly_report = annual_monthly_report.fillna(value={"Category": "Total"}, limit=1).sort_values(by=['Amount'], ascending=False)
+with pd.option_context("display.max_rows", 1000):   
+    if is_notebook():            
+        display(HTML(annual_monthly_report.to_html(index=False, header=True)))
+    else:
+        annual_monthly_report.to_html(open('report.html','w'), index=False, header=True)
+
+
+# In[170]:
+
+
+# Bar Chart of Annual Category Report
+#category_report.plot.bar(x='Category', y='Amount', figsize=(20,10),  yticks=(100, 250, 500, 1000, 1500, 2000, 2500, 5000))
+
+
+# In[171]:
+
+
+category_report['Amount'].sum()
+
+
+# In[172]:
+
+
+merchant_report = df.groupby(["Merchant"])['RAmount'].sum().reset_index(name='Amount').sort_values(by=['Amount'], ascending=False)
+#with pd.option_context("display.max_rows", 1000):
+#    display(HTML(merchant_report.to_html(index=False, header=True)))
+
+
+# In[173]:
+
+
+monthly_merchant_report = df.groupby([df.Date.dt.to_period('M'), "Merchant"])['RAmount'].sum().reset_index(name='Amount').sort_values(by=['Date', 'Amount'], ascending=False)
+#monthly_report
+
+
+# In[174]:
+
+
+pv_merchant_table = pd.pivot_table(monthly_merchant_report, index = 'Merchant', columns = 'Date', values = 'Amount', fill_value=0, dropna=False)
+#fig = pv_merchant_table.plot(kind = 'bar', figsize=(20,10),  yticks=(100, 250, 500, 1000, 1500, 2000, 2500, 5000))
+
+
+# In[175]:
+
+
+annual_monthly_merchant_report=merchant_report.merge(pv_merchant_table, on='Merchant')
+annual_monthly_merchant_report.loc['Total'] = annual_monthly_merchant_report.sum(numeric_only=True)
+annual_monthly_merchant_report = annual_monthly_merchant_report.fillna(value={"Merchant": "All"}, limit=1)
+
+
+with pd.option_context("display.max_rows", 1000):
+#    display(HTML(annual_monthly_merchant_report
+#                .sort_values(by=['Amount'], ascending=False)
+#                 .to_html(index=False, header=True)))
+    pass
+
